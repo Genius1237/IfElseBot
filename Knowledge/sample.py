52731629bccc3375eed2c09203099c52019f306b
nitishplus98 <nitishplus98@gmail.com>
2018-01-15 16:35:44 +0530

    Update sample.py to fix errors

--- Knowledge/sample.py
+++ Knowledge/sample.py
@@ -26,19 +26,23 @@ while True:
     elif message == "save":
         kernel.saveBrain("bot_brain.brn")
     else:
+        bot_response=""
+        kernel.setPredicate('ctype',0)
         bot_response = kernel.respond(message)
         if(len(bot_response)<2):
             bot_response="i don't know what you are talking about"
             print(bot_response)
             continue
         print(bot_response)
-        time.sleep(1)
         inp = kernel.getPredicate('ctype')
         if(inp=="1"):
+            time.sleep(1)
             kernel.setPredicate('expression',str(evaulateExpression(kernel.getPredicate('expression'))))
             bot_response = kernel.respond('results')
+            print(bot_response)
 
         elif(inp=="2"):
+            time.sleep(1)
             num = random.randint(0,1)
             if(num==1):
                 kernel.setPredicate('expression',"Heads")
@@ -46,9 +50,11 @@ while True:
                 kernel.setPredicate('expression',"Tails")
 
             bot_response = kernel.respond('toss outcome')
+            print(bot_response) 	
 
 
         elif(inp=='3' or inp=='4'):
+            time.sleep(1)
             uval = 6
             num = random.randint(1,6)
             if(inp=="4"):
@@ -56,12 +62,8 @@ while True:
                 num = random.randint(1,uval)
             kernel.setPredicate('expression',str(num))
             bot_response = kernel.respond('die outcome')
+            print(bot_response)
 
-        else:
-            bot_response=""
-
-            #print(bot_response)
-        if(len(bot_response)<2):
+        elif(len(bot_response)<2):
             bot_response="i don't know what you are talking about"
-       
-        print(bot_response)
+            print(bot_response)
